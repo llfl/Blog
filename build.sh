971b320300ee2c109b3fc149231d34eac5652abf
Kun <quinnHugh@hotmail.com>
2019-08-25 16:55:58 +0800

    update

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-docker run --rm --name hexo-deploy -i \
-           -v `pwd`/source:/hexo/source\
-           -v `pwd`/themes:/hexo/themes\
-           -v `pwd`/public:/hexo/public\
-           -v `pwd`/_config.yml:/hexo/_config.yml\
-           imux/hexo g
+if [ "$1" = 'g' ] || [ "$1" = 'generate' ] || [ "$1" = '' ]; then
+    docker run --rm --name hexo-deploy -i \
+               -v `pwd`/source:/hexo/source\
+               -v `pwd`/themes:/hexo/themes\
+               -v `pwd`/public:/hexo/public\
+               -v `pwd`/_config.yml:/hexo/_config.yml\
+               imux/hexo g
+fi
 
-# docker run --rm --name hexo-deploy -it \
-#            -p 8080:8080\
-#            -v `pwd`/source:/hexo/source\
-#            -v `pwd`/themes:/hexo/themes\
-#            -v `pwd`/public:/hexo/public\
-#            -v `pwd`/_config.yml:/hexo/_config.yml\
-#            imux/hexo
+if [ "$1" = 'n' ] || [ "$1" = 'new' ]; then
+    docker run --rm --name hexo-deploy -i \
+               -v `pwd`/source:/hexo/source\
+               imux/hexo n $2
+fi
