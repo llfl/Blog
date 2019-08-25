a1dcb163f92bde1f8b8163abf3d61a88e3a00d6c
Kun <quinnHugh@hotmail.com>
2019-08-25 15:47:21 +0800

    update

--- build.sh
+++ build.sh
@@ -5,4 +5,12 @@ docker run --rm --name hexo-deploy -i \
            -v `pwd`/themes:/hexo/themes\
            -v `pwd`/public:/hexo/public\
            -v `pwd`/_config.yml:/hexo/_config.yml\
-           imux/hexo d
+           imux/hexo g
+
+# docker run --rm --name hexo-deploy -it \
+#            -p 8080:8080\
+#            -v `pwd`/source:/hexo/source\
+#            -v `pwd`/themes:/hexo/themes\
+#            -v `pwd`/public:/hexo/public\
+#            -v `pwd`/_config.yml:/hexo/_config.yml\
+#            imux/hexo
